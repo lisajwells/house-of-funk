d0eee531b3e82c0aa7726b3783339673b79a15b3
Lisa Wells <lisawells999@gmail.com>
2016-07-20 14:03:45 -0400

    add thumbs to portfolio template, floated

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,5 +5,41 @@
 
 remove_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_subnav' );
 
+add_action('genesis_after_content', 'hf_add_portfolio_thumbs');
+function hf_add_portfolio_thumbs() {
+?>
+    <div class="port-thumbs">
 
+        <div class="port-thumb">
+            <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
+        </div>
+
+        <div class="port-thumb">
+            <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
+        </div>
+
+        <div class="port-thumb">
+            <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
+        </div>
+
+        <div class="port-thumb">
+            <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
+        </div>
+
+        <div class="port-thumb">
+            <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
+        </div>
+
+    </div>
+
+
+<?php }
+
+
+
+?>
+
+
+
+<?php
 genesis();
