81c2ff23e33badf9478eccd78d0a4c7f539fbf1b
Lisa Wells <lisawells999@gmail.com>
2016-07-20 15:45:08 -0400

    transfer port-thumb properties to port-thumbs a

--- page-portfolio.php
+++ page-portfolio.php
@@ -14,40 +14,40 @@ function hf_add_portfolio_thumbs() {
 ?>
     <div class="port-thumbs">
 
-        <div class="port-thumb">
+        <a href="<?php echo get_site_url(); ?>"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Can a Barn be Glamorous?
             </div>
-        </div>
+        </div></a>
 
-        <div class="port-thumb">
+        <a href="#"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Moderne Updated
             </div>
-        </div>
+        </div></a>
 
-        <div class="port-thumb">
+        <a href="#"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Livable Luxury
             </div>
-        </div>
+        </div></a>
 
-        <div class="port-thumb">
+        <a href="#"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 1700’s Farmhouse
             </div>
-        </div>
+        </div></a>
 
-        <div class="port-thumb">
+        <a href="#"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Casual Elegance
             </div>
-        </div>
+        </div></a>
 
     </div>
 
