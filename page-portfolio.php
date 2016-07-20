5eb25b54f31d8eaa4ac877f52d6efab9f64f7e10
Lisa Wells <lisawells999@gmail.com>
2016-07-20 15:52:33 -0400

    portfolio: close gap between gallery and thumbs

--- page-portfolio.php
+++ page-portfolio.php
@@ -14,35 +14,35 @@ function hf_add_portfolio_thumbs() {
 ?>
     <div class="port-thumbs">
 
-        <a href="<?php echo get_site_url(); ?>"><div class="port-thumb">
+        <a href="<?php echo get_site_url(); ?>/portfolio/can-barn-glamorous"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Can a Barn be Glamorous?
             </div>
         </div></a>
 
-        <a href="#"><div class="port-thumb">
+        <a href="<?php echo get_site_url(); ?>/portfolio/moderne-updated"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Moderne Updated
             </div>
         </div></a>
 
-        <a href="#"><div class="port-thumb">
+        <a href="<?php echo get_site_url(); ?>/portfolio/livable-luxury"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Livable Luxury
             </div>
         </div></a>
 
-        <a href="#"><div class="port-thumb">
+        <a href="<?php echo get_site_url(); ?>/portfolio/1700s-farmhouse"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 1700’s Farmhouse
             </div>
         </div></a>
 
-        <a href="#"><div class="port-thumb">
+        <a href="<?php echo get_site_url(); ?>/portfolio/casual-elegance"><div class="port-thumb">
             <img class="port-thumb-img" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/funk-design-studio-portfolio-interiors-styles-3-180x180.jpg">
             <div class="port-thumb-title">
                 Casual Elegance
