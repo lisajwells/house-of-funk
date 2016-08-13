7bc4e75a9e9055f55964883ec5bae72ddc726798
Lisa Wells <lisawells999@gmail.com>
2016-08-13 13:52:32 -0400

    ports: ran in <a>s in template; 2-across for mobile

--- page-portfolio.php
+++ page-portfolio.php
@@ -19,30 +19,22 @@ function hf_add_portfolio_thumbs() {
             <div class="port-thumb-title">
                 Glamorous Barn
             </div>
-        </div></a>
-
-        <a href="<?php echo get_site_url(); ?>/portfolio/modern-updated"><div class="port-thumb">
+        </div></a><a href="<?php echo get_site_url(); ?>/portfolio/modern-updated"><div class="port-thumb">
             <img class="port-thumb-img" nopin="nopin" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/moderne-updated.jpg">
             <div class="port-thumb-title">
                 Modern Updated
             </div>
-        </div></a>
-
-        <a href="<?php echo get_site_url(); ?>/portfolio/livable-luxury"><div class="port-thumb">
+        </div></a><a href="<?php echo get_site_url(); ?>/portfolio/livable-luxury"><div class="port-thumb">
             <img class="port-thumb-img" nopin="nopin" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/livable-luxury.jpg">
             <div class="port-thumb-title">
                 Livable Luxury
             </div>
-        </div></a>
-
-        <a href="<?php echo get_site_url(); ?>/portfolio/1700s-farmhouse"><div class="port-thumb">
+        </div></a><a href="<?php echo get_site_url(); ?>/portfolio/1700s-farmhouse"><div class="port-thumb">
             <img class="port-thumb-img" nopin="nopin" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/1700s-farmhouse-1.jpg">
             <div class="port-thumb-title">
                 1700’s Farmhouse
             </div>
-        </div></a>
-
-        <a href="<?php echo get_site_url(); ?>/portfolio/casual-elegance"><div class="port-thumb">
+        </div></a><a href="<?php echo get_site_url(); ?>/portfolio/casual-elegance"><div class="port-thumb">
             <img class="port-thumb-img" nopin="nopin" src="http://staging.houseoffunk.flywheelsites.com/wp-content/uploads/2016/07/casual-elegance.jpg">
             <div class="port-thumb-title">
                 Casual Elegance
